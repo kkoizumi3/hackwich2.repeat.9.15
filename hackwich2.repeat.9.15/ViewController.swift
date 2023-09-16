b47b2e3b42f3f6d54e66a4d5cee50901753e4f53
Kaci Koizumi <koizumi3@hawaii.edu>
2023-09-15 23:34:28 -1000

    Did Hackwich Part 1 & Part 2 up to step 4-6

--- hackwich2.repeat.9.15/ViewController.swift
+++ hackwich2.repeat.9.15/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //Part 8: where declare three string var
+       var sentOne = "I did"
+       var sentTwo = " part 8!"
+       var concatThree = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        concatThree = sentOne + sentTwo
+
     }
 
-
+    @IBAction func partEightButton(_ sender: Any)
+    {
+        print(concatThree)
+    }
+    
+    @IBAction func pressMeButtonPressed(_ sender: Any)
+    {
+        print("yippee")
+    }
 }
 
